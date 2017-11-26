0154c8500b7f515ddb8023112b2eca9a235ef281
d-akihiro <d_akihiro@outlook.com>
2017-11-26 12:14:02 +0900

    [add]finished Chapter7
    * ログイン画面作成
    * ログイン状態によるヘッダ表示切り替えを実装
    * ユーザ情報画面を表示する権限チェックを実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   root 'static_pages#home'
   #get '/users/:id', to: 'users#show'
 
   resources :users, only: [:new, :show, :create]
+
+  get '/login', to:'sessions#new'
+  post '/login', to:'sessions#create'
+  delete '/logout', to:'sessions#destroy'
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,11 +2,28 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   def setup
-    @user = users(:akihiro)
+    @user       = users(:akihiro)
+    @other_user       = users(:michael)
   end
 
-  test 'should show user' do
+  test 'should show user when logged in' do
+    post login_path, params: { session: { email: @user.email,
+                                          password: 'password' }}
     get user_path(@user)
+    assert flash.empty?
     assert_template 'users/show'
+  end
+
+  test 'should redirect user when not logged in' do
+    get user_path(@user)
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test 'should show user when other user logged in' do
+    post login_path, params: { session: { email: @other_user.email,
+                                          password: 'password' }}
+    get user_path(@user)
+    assert_redirected_to root_url
   end
 end
